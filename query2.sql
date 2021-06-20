dd76e48d5e7b27b15c2f5bb055c2e4aa6844ac28
Aby George <abygeorge8848@gmail.com>
2021-06-20 23:50:55 +0530

    fixed another dumb error

--- query2.sql
+++ query2.sql
@@ -1,1 +1,1 @@
-SELECT  b.title, p.name FROM books b, publisher p publisher WHERE b.publisher = p.id AND p.country IN ('UK');
+SELECT  b.title, p.name FROM books b, publisher p WHERE b.publisher = p.id AND p.country IN ('UK');
