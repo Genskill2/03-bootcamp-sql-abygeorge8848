f921bdc09bf2aa6255ccc7d14cc0df84d3157b1f
Aby George <abygeorge8848@gmail.com>
2021-06-20 22:28:34 +0530

    5th change to create.sql

--- create.sql
+++ create.sql
@@ -1,21 +1,25 @@
 CREATE TABLE publisher(
 	id INTEGER PRIMARY KEY,
 	name text,
-	country text);
+	country text
+	);
 
 CREATE TABLE books(
 	id INTEGER PRIMARY KEY,
 	title text,
-	publisher INTEGER REFERENCES publisher(id));
+	publisher INTEGER REFERENCES publisher(id)
+	);
 	
 
 CREATE TABLE subjects(
 	id INTEGER PRIMARY KEY,
-	name text);
+	name text
+	);
 
 CREATE TABLE books_subjects(
 	book INTEGER REFERENCES subjects(id), 
-	subject INTEGER REFERENCES books(id));
+	subject INTEGER REFERENCES books(id)
+	);
 	
 
 
