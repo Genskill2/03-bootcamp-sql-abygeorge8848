bbfbe053edd0ddff6d3b94f7a7e9efaddb914fb6
Aby George <abygeorge8848@gmail.com>
2021-06-20 23:01:49 +0530

    query2 2nd commit

--- query2.sql
+++ query2.sql
@@ -1,1 +1,1 @@
-SELECT  b.title, p.name FROM books b, publisher p WHERE b.publisher = p.id AND p.country IN ('UK');
+SELECT  b.title, p.name FROM books b INNER JOIN publisher p WHERE b.publisher = p.id AND p.country IN ('UK');
